c23f89bce1c27f255437a761ea34f17e56fe9421
Vedang Barman <vedangbarman@gmail.com>
2026-07-12 14:39:26 +0530

    adding file

---
+++ src/data_cleaning/schema_print.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+DB_PATH = "data/raw/pr_data.db"
+
+
+def main():
+    conn = sqlite3.connect(DB_PATH)
+    cur = conn.cursor()
+    
+    
+    schema_repos = conn.execute("pragma table_info(repos);")
+    schema_pull_requests = conn.execute("pragma table_info(pull_requests);")
+    schema_pr_labels = conn.execute("pragma table_info(pr_labels);")
+    
+    print("Schema for 'repos' table:")
+    for rows_repos in schema_repos:
+        print(rows_repos)
+    
+    print("Schema for 'pull_requests' table:")
+    for rows_pull_requests in schema_pull_requests:
+        print(rows_pull_requests)
+    
+    print("Schema for 'pr_labels' table:")
+    for rows_pr_labels in schema_pr_labels:
+        print(rows_pr_labels)
+
+    conn.close()
+    
+
+if __name__ == "__main__":
+    main()
