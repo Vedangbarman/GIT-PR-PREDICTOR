0f8287fef55df36f4114681e394b2f24d3058d75
Vedang Barman <vedangbarman@gmail.com>
2026-07-13 13:20:20 +0530

    excluded pytorch because reasons I forgot

---
+++ src/data_cleaning/exclude_pytorch.py
@@ -0,0 +1,21 @@
+import sqlite3 
+
+
+DB_PATH = "data/raw/pr_data.db"
+
+EXCLUSION = {
+    ("pytorch","pytorch"): "pytorchbot_label_based_merge"
+}
+
+def main():
+    conn = sqlite3.connect(DB_PATH)
+    
+    for (owner,name), reason in EXCLUSION.items():
+        conn.execute(
+            "UPDATE repos SET exclude_reason = ? WHERE owner == ? AND name = ?",(reason,owner,name),
+        )
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
